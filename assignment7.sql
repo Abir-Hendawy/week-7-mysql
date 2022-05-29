c26c7e9436a499f0ad958c0b7f000d00c62de2cc
Abir-Hendawy <90090782+Abir-Hendawy@users.noreply.github.com>
2022-05-29 17:06:52 +0200

    Create assignment7.sql

---
+++ assignment7.sql
@@ -0,0 +1,28 @@
+
+-- question 1
+SELECT `orderdetails`.`orderNumber` AS `orderNumber`,
+sum(`orderdetails`.`quantityOrdered` * `orderdetails`.`priceEach`) AS `total_Payment` FROM `orderdetails`
+GROUP BY `orderdetails`.`orderNumber``orderNumber`;
+
+-- question 2
+SELECT concat(`emp`.`firstName`,' ',`emp`.`lastName`) 
+AS `fullName_Employee`, concat(`mang`.`firstName`,' ',`mang`.`lastName`) AS `fullName_Manager` 
+FROM (`employees` `emp` join `employees` `mang` 
+on(`mang`.`employeeNumber` = `emp`.`reportsTo`))  ;
+
+
+-- question 3
+select `employees`.`employeeNumber` AS `employeeNumber`,`employees`.`firstName` AS `firstName`,`employees`.`lastName` AS `lastName` from `employees` where `employees`.`officeCode` = (select `offices`.`officeCode` 
+from `offices` where `offices`.`city` = 'london';
+
+-- question 4
+SELECT `e`.`employeeNumber` AS `employeeNumber`, `e`.`firstName` AS `firstName`, `e`.`lastName` AS `lastName`, `o`.`city` AS `city` FROM (`employees` `e` join `offices` `o` 
+on(`e`.`officeCode` = `o`.`officeCode` and `o`.`city` = 'london')
+
+
+-- question 5
+select c.customerNumber,c.customerName,o.orderNumber,sum(od.quantityOrdered*od.priceEach)total_payment
+from customers c join orders o join orderdetails od
+on(c.customerNumber=o.customerNumber)
+and(o.orderNumber=od.orderNumber)
+group by od.orderNumber
